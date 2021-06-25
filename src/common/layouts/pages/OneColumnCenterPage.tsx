149058117a58187ca652a5b074068aaaee94765f
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-25 23:33:44 +0530

    dashboard layout and single column centered layout

--- src/common/layouts/pages/OneColumnCenterPage.tsx
+++ src/common/layouts/pages/OneColumnCenterPage.tsx
@@ -1,10 +1,12 @@
-import {Container} from '@chakra-ui/react';
+import {Container, SimpleGrid} from '@chakra-ui/react';
 import React from 'react';
 
 export default function OneColumnCenterPage<T>(props: React.PropsWithChildren<T>){
     return (
         <Container maxW="container.lg" centerContent>
-            {props.children}
+            <SimpleGrid columns={2}>
+                {props.children}
+            </SimpleGrid>
         </Container>
     )
 }
