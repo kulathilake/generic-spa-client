5de2211e2cb9c49edb10f4c8d4cb9c553fd300ca
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-26 09:36:27 +0530

    Landing Page

--- src/features/landing/index.tsx
+++ src/features/landing/index.tsx
@@ -2,16 +2,23 @@ import ColumnedPage, {Column, Row} from "../../common/layouts/pages/ColumnedPage
 
 export default function LandingPage(){
     return (
-        <ColumnedPage gap={2} >
+        <ColumnedPage gap={2} fullWidth={true} >
             <Column>
                 <Row>
                     {/* Here Goes the Main Menu */}
+                    Landing Page Menu.
                 </Row>
                 <Row>
                     {/* Here Goes the Carousel */}
+                    Carousel.
                 </Row>
                 <Row>
-                    {/*  */}
+                    {/* Body of the Landing Page */}
+                    Body.
+                </Row>
+                <Row>
+                    {/* Here Goes the Footer */}
+                    Landing Page Footer.
                 </Row>
             </Column>
         </ColumnedPage>
