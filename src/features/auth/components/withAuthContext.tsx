9fc61eadf8e80c198af201fe18a959dfebcbe135
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-27 01:20:13 +0530

    Added Alerts for Login and Register errors

--- src/features/auth/components/withAuthContext.tsx
+++ src/features/auth/components/withAuthContext.tsx
@@ -2,24 +2,34 @@ import React from "react";
 import { AuthWrapperProps } from "../types/types";
 import AuthApi from '../api';
 import { Roles } from "../../../common/types/auth";
+import { useAlert } from "../../../app/hooks";
 
 export default function withAuthContext <T extends AuthWrapperProps>(Component: React.ComponentType<T>)
 : React.FC<Omit<T, keyof AuthWrapperProps>>{
     
     return function AuthComponent(props){
         const api = AuthApi.getApi();
+        const {setAlert} = useAlert();
         const handleLogin = async (email:string, password:string) => {
             try{
                 return await api.login(email,password)
             }catch(error){
-                console.log(error);
+                setAlert({
+                    message: error.message,
+                    severity: 'error',
+                    show: true,
+                });
             }
         };
         const handleSignup = async (name: string, email: string, password: string, role: Roles) => {
             try{
                 return await api.signup(email, password);
             }catch(error){
-                console.log(error);
+                setAlert({
+                    message: error.message,
+                    severity: 'error',
+                    show: true,
+                });
             }
         }
         return <Component 
