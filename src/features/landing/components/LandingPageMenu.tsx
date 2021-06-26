96365fe44ae495c6138e98d97736aa966168d9e1
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-26 09:41:16 +0530

    Landing page component skeleton

---
+++ src/features/landing/components/LandingPageMenu.tsx
@@ -0,0 +1,17 @@
+export default function LandingPageMenu(){
+    return (
+        <nav>
+            <div>
+                <img src="" alt="outreach logo"/>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Features</li>
+                <li>Pricing</li>
+            </ul>
+            <div>
+                Login/Register
+            </div>
+        </nav>
+    )
+}
