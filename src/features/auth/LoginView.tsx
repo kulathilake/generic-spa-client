fa0f84eb5b573f2998fbfa22253d2c68010c49a0
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-26 10:44:43 +0530

    Login form control and form submission handler;

--- src/features/auth/LoginView.tsx
+++ src/features/auth/LoginView.tsx
@@ -1,11 +1,23 @@
 import { LoginViewProps } from "./types/types";
 import withAuthContext from "./components/withAuthContext";
 import ColumnedPage, {Column, Row} from "../../common/layouts/pages/ColumnedPage";
+import React, { FormEventHandler } from "react";
 
 /**
  * UI For Login Screen
  */
 export function LoginView(props: LoginViewProps) {
+
+    const handleLoginSubmit: FormEventHandler = (e: React.FormEvent<HTMLFontElement>) =>{
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+            email: {value: string},
+            password: {value: string};
+        }
+        props.onLogin(target.email.value, target.password.value);
+    };
+
+
     return (
         <ColumnedPage center={true}>
             <Column>
@@ -13,12 +25,24 @@ export function LoginView(props: LoginViewProps) {
                     Login
                 </Row>
                 <Row>
-                    <form>
+                    <form onSubmit={handleLoginSubmit}>
                         <input id="email" name="email"/>
                         <input id="password" name="password" type="password"/>
                         <button type="submit">Login</button>
                     </form>
                 </Row>
+                <Row>
+                    Don't Have an Account yet?
+                    <a href="/register">Create an Account</a>
+                </Row>
+                <Row>
+                    <a href="/login/reset">Forgot Password?</a>
+                </Row>
+                <Row>
+                    <button>Login with Google</button>
+                    <button>Login with Facebook</button>
+                </Row>
+                
             </Column>
         </ColumnedPage>
     )
