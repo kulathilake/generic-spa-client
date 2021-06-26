6fc3a3c21e3f5c611482366f7aa9485c54cc106d
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-26 10:49:05 +0530

    Auth Wrapper Calls the Login API

--- src/features/auth/components/withAuthContext.tsx
+++ src/features/auth/components/withAuthContext.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { AuthWrapperProps } from "../types/types";
+import AuthApi from '../api';
 
 export default function withAuthContext <T extends AuthWrapperProps>(Component: React.ComponentType<T>)
 : React.FC<Omit<T, keyof AuthWrapperProps>>{
+    
     return function AuthComponent(props){
+        const api = AuthApi.getApi();
         const handleLogin = (email:string, password:string) => {
-            
+            api.login(email,password);
         };
         return <Component 
         {...(props as T)} 
