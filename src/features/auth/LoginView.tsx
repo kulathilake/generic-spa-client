378f7c9b487c84bbb4f16d76bee768075f8f0ff2
Shehan Kulathilake <shehanhere@gmail.com>
2021-06-26 10:32:02 +0530

    Login View Skeleton

--- src/features/auth/LoginView.tsx
+++ src/features/auth/LoginView.tsx
@@ -1,11 +1,27 @@
 import { LoginViewProps } from "./types/types";
 import withAuthContext from "./components/withAuthContext";
+import ColumnedPage, {Column, Row} from "../../common/layouts/pages/ColumnedPage";
 
 /**
  * UI For Login Screen
  */
 export function LoginView(props: LoginViewProps) {
-    return null;
+    return (
+        <ColumnedPage center={true}>
+            <Column>
+                <Row>
+                    Login
+                </Row>
+                <Row>
+                    <form>
+                        <input id="email" name="email"/>
+                        <input id="password" name="password" type="password"/>
+                        <button type="submit">Login</button>
+                    </form>
+                </Row>
+            </Column>
+        </ColumnedPage>
+    )
 };
 
 export default withAuthContext<LoginViewProps>(LoginView);
